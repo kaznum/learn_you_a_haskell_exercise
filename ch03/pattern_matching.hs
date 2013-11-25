a2fdbf6ede85bbcfa0fbaafe8bc8680f1d589432
Kazuya NUMATA <numata@gmail.com>
2013-11-25 13:37:19 +0900

    Implement 'ch03/pattern_matching' examples

--- ch03/pattern_matching.hs
+++ ch03/pattern_matching.hs
@@ -21,4 +21,51 @@ charName 'c' = "Cecil"
 
 -- Pattern Matching with Tuples
 
---- to be continued
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors a b = (fst a + fst b, snd a + snd b)
+
+addVectors' :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors' (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- :t addVectors'
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+second :: (a, b, c) -> b
+second (_, y, _) = y
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+-- Pattern Matching with Lists and List Comprehensions
+
+-- let xs = [(1,2),(3,4),(5,6),(7,8),(9,10)]
+-- [a+b|(a, b) <- xs]
+head' :: [a] -> a
+head' [] = error "Cannot call head on an empty list, dummy!"
+head' (x:_) = x
+
+-- head' [4,5,6]
+-- head' "Hello"
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "The list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+-- tell ['a', 'b']
+-- tell ['a']
+-- tell [1]
+-- tell [1,2,3]
+-- tell []
+
+badAdd :: (Num a) => [a] -> a
+badAdd (x:y:z:[]) = x + y + z
+
+-- badAdd [100, 20]
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+-- firstLetter "hello"
