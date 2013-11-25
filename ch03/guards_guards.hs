ddc9d90d6e643d8429dc5243607473ef77a9dace
Kazuya NUMATA <numata@gmail.com>
2013-11-25 13:58:39 +0900

    Implement "ch03/guards_guards" examples

--- ch03/guards_guards.hs
+++ ch03/guards_guards.hs
@@ -5,4 +5,22 @@ bmiTell bmi
   | bmi <= 30.0 = "You're fat"
   | otherwise = "You're whale, congrats!"
 
--- to be continued
+bmiTell' :: Double -> Double -> String
+bmiTell' weight height
+  | weight / height ^ 2 <= 18.5 = "You're underwight, you emo, you!"
+  | weight / height ^ 2 <= 25.0 = "You're supposedly normal."
+  | weight / height ^ 2 <= 30.0 = "You're fat"
+  | otherwise = "You're whale, congrats!"
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a <= b = b
+  | otherwise = a
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a == b = EQ
+  | a <= b = LT
+  | otherwise = GT
+
+-- 3 `myCompare` 2
