f510b6f2aec5b04cab244f019f2c941cc1bad058
Kazuya Numata <numata@gmail.com>
2013-11-27 14:14:55 +0900

    Implement ch05/the_functional_programmers_toolbox examples (in progress)

--- ch05/the_functional_programmers_toolbox.hs
+++ ch05/the_functional_programmers_toolbox.hs
@@ -8,5 +8,27 @@ map' f (x:xs) = f x : map' f xs
 -- map' fst [(1,2),(3,5),(6,3),(2,6),(2,5)]
 
 --- The filter Function
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs)
+        | f x = x : filter' f xs
+        | otherwise = filter' f xs
+
+-- filter' (>3) [1,2,3,4,5,6]
+-- filter' (==3) [1,2,3,4,5]
+-- let notNull x = not (null x) in filter' notNull [[1,2,3],[],[3,4,5],[2,2],[],[]]
+
+-- filter' (`elem` ['a'..'z']) "u dafKdfa dfaiKSDF"
+-- filter' (<15) (filter' even [1..20])
+-- [x| x <- [1..20], x < 15, even x]
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+          let smallerOrEqual = filter' (<= x) xs
+              larger = filter' (> x) xs
+          in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+--- More Examples of map and filter
 
 -- to be continued
