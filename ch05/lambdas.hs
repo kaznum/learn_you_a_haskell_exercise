3aba6279c908c3d76e14ef8bd5624b23a8c5b5ed
Kazuya Numata <numata@gmail.com>
2013-11-28 22:17:29 +0900

    Implement ch05/lambdas examples

--- ch05/lambdas.hs
+++ ch05/lambdas.hs
@@ -1,7 +1,30 @@
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+      | even n = n : chain (n `div` 2)
+      | odd n = n : chain (n*3 + 1)
+
 numLongChains :: Int
 numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
 
 -- map (+3) [1,6,3,2]
 -- map (\x -> x + 3) [1,6,3,2]
 
+-- zipWith (\a b -> (a * 30 + 3) / b) [5,4,3,2,1] [1,2,3,4,5]
+-- map (\(a,b) -> a + b) [(1,2),(3,5),(6,3)]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+-- addThree 1 2 3
+
+addThree' :: Int -> Int -> Int -> Int
+addThree' = \x -> \y -> \z -> x + y + z
+-- addThree' 1 2 3
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f = \x y -> f y x
+-- flip' (^) 2 3
+
+-- zipWith (flip (++)) ["love you", "love me"] ["i ", "you "]
+-- map (flip subtract 20) [1,2,3,4]
 -- to be continued
