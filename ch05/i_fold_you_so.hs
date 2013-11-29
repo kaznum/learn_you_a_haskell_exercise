1c89c677bb3edc8979bacb1e06088e5c0c533ef8
Kazuya NUMATA <numata@gmail.com>
2013-11-29 14:03:28 +0900

    Implement ch05/i_fold_you_so examples

--- ch05/i_fold_you_so.hs
+++ ch05/i_fold_you_so.hs
@@ -56,5 +56,19 @@ last' = foldl1 (\_ x -> x)
 -- foldl (flip (:)) [] [3,4,5,6]
 
 --- Folding Infinite Lists
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
 
--- to be conginued
+-- and' (repeat False)
+
+--- Scans
+-- scanl (+) 0 [3,5,2,1]
+-- scanr (+) 0 [3,5,2,1]
+-- scanl1 (\acc x -> if x > acc then x else acc) [3,4,5,3,7,9,2,1]
+-- scanl (flip (:)) [] [3,2,1]
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (< 1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+-- sqrtSums
+-- sum(map sqrt [1..131])
