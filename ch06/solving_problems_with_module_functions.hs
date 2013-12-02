df21b8589f47a7239cdca28cefe69c94958f4b79
Kazuya NUMATA <numata@gmail.com>
2013-12-02 10:23:44 +0900

    Implement "ch06/solving_problems_with_module_functions" examples (in progress)

--- ch06/solving_problems_with_module_functions.hs
+++ ch06/solving_problems_with_module_functions.hs
@@ -1,6 +1,6 @@
 --- Counting Words
 
-import Data.List (words, group, sort)
+import Data.List (words, group, sort, tails, isPrefixOf, any, isInfixOf)
 
 -- words "hey these are the words in this sentence"
 -- words "hey    these    are  the words in this sentence"
@@ -15,5 +15,25 @@ wordNums :: String -> [(String, Int)]
 wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
 
 --- Needle in the Haystack
+
+-- tails "party"
+-- tails [1,2,3]
+
+-- "hawaii" `isPrefixOf` "hawaii joe"
+-- "haha" `isPrefixOf` "ha"
+-- "ha" `isPrefixOf` "ha"
+-- any (> 4) [1,2,3]
+-- any (== 'F') "Frank Sobotka"
+-- any (\x -> x > 5 && x < 10) [1,4,11]
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+-- "art" `isIn` "party"
+-- [1,2] `isIn` [1,3,5]
+
+-- "art" `isInfixOf` "party"
+
+--- Caesar Cipher Salad
 
 -- to be continued
