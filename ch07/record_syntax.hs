4f94913fbf844823580277971fce378be2b09e06
Kazuya NUMATA <numata@gmail.com>
2013-12-03 16:40:06 +0900

    Implement ch07/record_syntax examples

---
+++ ch07/record_syntax.hs
@@ -0,0 +1,8 @@
+data Person = Person String String Int Float String String deriving (Show)
+-- Firstname, Lastname, age, height, phone, favorite ice cream flavor
+
+-- *Main> let guy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
+-- *Main> guy
+-- Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
+
+-- to be continued
