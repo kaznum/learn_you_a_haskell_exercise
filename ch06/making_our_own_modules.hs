8d50eeec09d5683efa7364dcbadc3e8be7da892f
Kazuya NUMATA <numata@gmail.com>
2013-12-03 14:35:24 +0900

    Implement ch06/making_our_own_modules examples

--- ch06/making_our_own_modules.hs
+++ ch06/making_our_own_modules.hs
@@ -1,3 +1,16 @@
 --- A Geometry Module
 
--- to be continued
+-- See Geometry.hs
+
+--- Hierarchical Modules
+
+-- See Geometry/*.hs
+
+import qualified Geometry.Sphere as Sphere
+-- Sphere.volume 1
+import qualified Geometry.Cube as Cube
+-- Cube.volume 2
+-- Cube.area 2
+import qualified Geometry.Cuboid as Cuboid
+-- Cuboid.volume 2 3 4
+-- Cuboid.area 2 3 4
