53f9b759f9702297ca7df80ac48ccfbc74a13091
Kazuya NUMATA <numata@gmail.com>
2013-12-04 14:19:53 +0900

    Implement ch07/type_synonyms examples (in progress)

---
+++ ch07/type_synonyms.hs
@@ -0,0 +1,21 @@
+--- Making Our Phonebook Prettier
+
+phoneBook :: [(String, String)]
+phoneBook = [("betty", "555-2938")
+            ,("bonnie", "452-2928")
+            ,("patsy", "493-2928")
+            ,("lucille", "205-2928")
+            ,("wendy", "939-8282")
+            ,("penny", "853-2492")]
+
+-- type PhoneBook = [(String, String)]
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+--- Parameterizing Type Synonyms
+
+-- to be continued
