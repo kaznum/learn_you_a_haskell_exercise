2069b437059f1cd90c70e6b598bc3b99668069e7
Kazuya Numata <numata@gmail.com>
2013-12-11 22:03:52 +0900

    Implement ch07/recursive_data_structures examples (in progress)

--- ch07/recursive_data_structures.hs
+++ ch07/recursive_data_structures.hs
@@ -12,5 +12,25 @@ data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
 -- Cons 3 (Cons 4 (Cons 5 Empty))
 
 --- Improving Out List
+infixr 5 :-:
+data List' a = Empty' | a :-: (List' a) deriving (Show, Read, Eq, Ord)
+
+-- *Main> 3 :-: 4 :-: 5 :-: Empty'
+-- 3 :-: (4 :-: (5 :-: Empty'))
+-- *Main> let a = 3 :-: 4 :-: 5 :-: Empty'
+-- *Main> 100 :-: a
+-- 100 :-: (3 :-: (4 :-: (5 :-: Empty')))
+
+infixr 5 ^++
+(^++) :: List' a -> List' a -> List' a
+Empty' ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+-- *Main> let a = 3 :-: 4 :-: 5 :-: Empty'
+-- *Main> let b = 6 :-: 7 :-: Empty'
+-- *Main> a ^++ b
+-- 3 :-: (4 :-: (5 :-: (6 :-: (7 :-: Empty'))))
+
+--- Let's Plant a Tree
 
 -- to be continued
