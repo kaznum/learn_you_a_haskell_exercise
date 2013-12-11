468532e18a3647b175248fee8695d15d1de60cf5
Kazuya Numata <numata@gmail.com>
2013-12-11 23:13:53 +0900

    Implement ch07/recursive_data_structures examples

--- ch07/recursive_data_structures.hs
+++ ch07/recursive_data_structures.hs
@@ -32,5 +32,43 @@ Empty' ^++ ys = ys
 -- 3 :-: (4 :-: (5 :-: (6 :-: (7 :-: Empty'))))
 
 --- Let's Plant a Tree
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
 
--- to be continued
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+-- *Main> let nums = [8,6,4,1,7,3,5]
+-- *Main> let numsTree = foldr treeInsert EmptyTree nums
+-- *Main> numsTree
+-- Node 5
+--   (Node 3
+--     (Node 1 EmptyTree EmptyTree)
+--     (Node 4 EmptyTree EmptyTree)
+--   )
+--   (Node 7
+--     (Node 6 EmptyTree EmptyTree)
+--     (Node 8 EmptyTree EmptyTree)
+--   )
+
+-- *Main> 8 `treeElem` numsTree
+-- True
+-- *Main> 100 `treeElem` numsTree
+-- False
+-- *Main> 1 `treeElem` numsTree
+-- True
+-- *Main> 10 `treeElem` numsTree
+-- False
