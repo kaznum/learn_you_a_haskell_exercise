ef017a062f8554b97fbdc8cf0b3cc5ff32fd1eea
Kazuya NUMATA <numata@gmail.com>
2013-12-12 19:33:19 +0900

    Implement ch07/type_classes_102 examples (in progress)

--- ch07/type_classes_102.hs
+++ ch07/type_classes_102.hs
@@ -1,3 +1,27 @@
 --- Inside the Eq Type Class
 
+--- A Traffic Light Data Type
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Yellow = "Yellow light"
+  show Green = "Green light"
+
+-- *Main> Red == Red
+-- True
+-- *Main> Red == Yellow
+-- False
+-- *Main> Red `elem` [Red, Yellow, Green]
+-- True
+-- *Main> [Red, Yellow, Green]
+-- [Red light,Yellow light,Green light]
+
+--- Subclassing
+
 -- to be continued
