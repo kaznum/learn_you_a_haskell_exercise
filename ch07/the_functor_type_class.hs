fd6c3b1ffb7e61053e6f5978131ea856d7d9d3ca
Kazuya NUMATA <numata@gmail.com>
2013-12-13 16:15:42 +0900

    Implement ch07/the_functor_type_class examples (in progress)

---
+++ ch07/the_functor_type_class.hs
@@ -0,0 +1,12 @@
+-- *Main> fmap (*2) [1..3]
+-- [2,4,6]
+-- *Main> map (*2) [1..3]
+-- [2,4,6]
+-- *Main> fmap (*2) []
+-- []
+-- *Main> map (*2) []
+-- []
+
+--- Maybe As a Functor
+
+-- to be continued
