26e28ad7530f85499b95cae5703db868b4fc8ef7
Kazuya Numata <numata@gmail.com>
2013-12-20 00:49:49 +0900

    Implement ch08/gluing_io_actions_together examples (in progress)

--- ch08/gluing_io_actions_together.hs
+++ ch08/gluing_io_actions_together.hs
@@ -1,1 +1,12 @@
+-- see hey_you_rock.hs
+
+-- *Main> :t getLine
+-- getLine :: IO String
+
+-- see hey_you_rock2.hs
+
+-- *Main> putStrLn "HEEY"
+-- HEEY
+
+--- Using let Inside I/O Actions
 -- to be continued
