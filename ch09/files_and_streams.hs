c12a79249c0dfd06f013312b996c107dab970be1
Kazuya Numata <numata@gmail.com>
2013-12-22 23:06:09 +0900

    Implement ch09/files_and_streams examples (in progress)

--- ch09/files_and_streams.hs
+++ ch09/files_and_streams.hs
@@ -2,5 +2,16 @@
 -- see haiku.txt and capslocker.hs
 
 --- Getting Strings from Input Streams
+-- see capslocker2.hs
+
+-- % ./capslocker2 
+-- hey ho
+-- HEY HO
+-- lets go
+-- LETS GO
+
+-- see shortlinesonly.hs and shortlines.txt
+
+--- Transforming Input
 
 -- to be continued
