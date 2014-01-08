b80c0d9b34fa8668c8b90ce565fc45acaed26271
Kazuya NUMATA <numata@gmail.com>
2014-01-08 13:08:04 +0900

    Implement ch09/randomness examples (in progress)

---
+++ ch09/randomness.hs
@@ -0,0 +1,30 @@
+-- *Main> import System.Random
+-- *Main System.Random> random (mkStdGen 100)
+
+-- <interactive>:6:1:
+--     No instance for (Random a0) arising from a use of `random'
+--     The type variable `a0' is ambiguous
+--     Possible fix: add a type signature that fixes these type variable(s)
+--     Note: there are several potential instances:
+--       instance Random Bool -- Defined in `System.Random'
+--       instance Random Foreign.C.Types.CChar -- Defined in `System.Random'
+--       instance Random Foreign.C.Types.CDouble
+--         -- Defined in `System.Random'
+--       ...plus 33 others
+--     In the expression: random (mkStdGen 100)
+--     In an equation for `it': it = random (mkStdGen 100)
+-- *Main System.Random> random (mkStdGen 100) :: (Int, StdGen)
+-- (-3650871090684229393,693699796 2103410263)
+-- *Main System.Random> random (mkStdGen 100) :: (Int, StdGen)
+-- (-3650871090684229393,693699796 2103410263)
+-- *Main System.Random> random (mkStdGen 949494) :: (Int, StdGen)
+-- (261237333865217244,75809027 2103410263)
+-- *Main System.Random> random (mkStdGen 949488) :: (Float, StdGen)
+-- (0.8241101,1597344447 1655838864)
+-- *Main System.Random> random (mkStdGen 949488) :: (Bool, StdGen)
+-- (False,1485632275 40692)
+-- *Main System.Random> random (mkStdGen 949488) :: (Integer, StdGen)
+-- (2779100082051559757,587416689 2103410263)
+
+--- Tossing a Coin
+-- to be continued
