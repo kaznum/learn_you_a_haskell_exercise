5aea62a436b028a4375a778a85506e8d5058144a
Kazuya NUMATA <numata@gmail.com>
2014-01-08 16:43:33 +0900

    Implement ch09/bytestrings examples (in progress)

--- ch09/bytestrings.hs
+++ ch09/bytestrings.hs
@@ -1,3 +1,36 @@
 --- Strict and Lazy Bytestrings
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString as S
+
+-- *Main> B.pack [99,97,110]
+-- "can"
+-- *Main> B.pack [98..120]
+-- "bcdefghijklmnopqrstuvwx"
+-- *Main> B.pack [336]
+-- "P"
+
+-- *Main> let by = B.pack [98,111,114,116]
+-- *Main> by
+-- "bort"
+-- *Main> B.unpack by
+-- [98,111,114,116]
+
+-- *Main> :t B.toChunks
+-- B.toChunks :: B.ByteString -> [S.ByteString]
+-- *Main> :t B.fromChunks
+-- B.fromChunks :: [S.ByteString] -> B.ByteString
+
+-- *Main> B.fromChunks [S.pack [40,41,42], S.pack [43,44,45], S.pack [46,47,48]]
+-- "()*+,-./0"
+
+-- *Main> B.cons 85 $ B.pack [80,81,82,84]
+-- "UPQRT"
+
+-- *Main> :t B.readFile
+-- B.readFile :: FilePath -> IO B.ByteString
+-- *Main> :t S.readFile
+-- S.readFile :: FilePath -> IO S.ByteString
+
+--- Copying Files with Bytestrings
 
 -- to be continued
