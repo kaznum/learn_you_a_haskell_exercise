fae1d3d67026f63d78a427021e8fd15be23377c9
Kazuya NUMATA <numata@gmail.com>
2014-01-08 17:38:35 +0900

    Implement ch10/reverse_polish_notation_calculator examples

--- ch10/reverse_polish_notation_calculator.hs
+++ ch10/reverse_polish_notation_calculator.hs
@@ -21,5 +21,24 @@ solveRPN = head . foldl foldingFunction [] . words
 -- 86.2
 
 --- Adding More Operators
+solveRPN' :: String -> Double
+solveRPN' = head . foldl foldingFunction [] . words
+  where foldingFunction (x:y:ys) "*" = (y * x):ys
+        foldingFunction (x:y:ys) "+" = (y + x):ys
+        foldingFunction (x:y:ys) "-" = (y - x):ys
+        foldingFunction (x:y:ys) "/" = (y / x):ys
+        foldingFunction (x:y:ys) "^" = (y ** x):ys
+        foldingFunction (x:xs) "ln" = log x:xs
+        foldingFunction xs "sum" = [sum xs]
+        foldingFunction xs numberString = read numberString:xs
+
+-- *Main> solveRPN' "2.7 ln"
+-- 0.9932517730102834
+-- *Main> solveRPN' "10 10 10 10 sum 4 /"
+-- 10.0
+-- *Main> solveRPN' "10 10 10 10 10 sum 4 /"
+-- 12.5
+-- *Main> solveRPN' "10 2 ^"
+-- 100.0
 
 -- to be continued
