3f56f7e9dcf55c1909c5286b38310ae87eb478e8
Kazuya Numata <numata@gmail.com>
2014-01-19 23:49:31 +0900

    Implement ch12/folding_with_monoids examples (in progress)

--- ch12/folding_with_monoids.hs
+++ ch12/folding_with_monoids.hs
@@ -1,1 +1,21 @@
+import qualified Data.Foldable as F
+
+-- *Main> :t foldr
+-- foldr :: (a -> b -> b) -> b -> [a] -> b
+-- *Main> :t F.foldr
+-- F.foldr :: F.Foldable t => (a -> b -> b) -> b -> t a -> b
+
+-- *Main> foldr (*) 1 [1,2,3]
+-- 6
+-- *Main> F.foldr (*) 1 [1,2,3]
+-- 6
+-- *Main> F.foldl (+) 2 (Just 9)
+-- 11
+-- *Main> F.foldr (+) 2 (Just 9)
+-- 11
+-- *Main> F.foldr (||) False (Just True)
+-- True
+-- *Main> F.foldl (||) False (Just True)
+-- True
+
 -- to be continued
