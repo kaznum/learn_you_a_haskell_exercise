4502365ddda389682f467278e3dd74b8e1d9fafb
Kazuya NUMATA <numata@gmail.com>
2014-01-21 13:26:37 +0900

    Implement ch13/upgrading_our_applicative_functors examples

--- ch13/upgrading_our_applicative_functors.hs
+++ ch13/upgrading_our_applicative_functors.hs
@@ -1,1 +1,9 @@
--- to be continued
+import Control.Applicative
+
+-- *Main> (*) <$> Just 2 <*> Just 8
+-- Just 16
+-- *Main> (++) <$> Just "klingon" <*> Nothing
+-- Nothing
+-- *Main> (-) <$> [3,4] <*> [1,2,3]
+-- [2,1,0,3,2,1]
+
