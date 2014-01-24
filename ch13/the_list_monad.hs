e9cc4b91dc9227852a957d4ffd69d3f4c2e94c61
Kazuya NUMATA <numata@gmail.com>
2014-01-24 23:42:22 +0900

    Implement ch13/the_list_monad examples (in progress)

--- ch13/the_list_monad.hs
+++ ch13/the_list_monad.hs
@@ -1,1 +1,18 @@
+-- *Main> import Control.Applicative
+-- *Main Control.Applicative> (*) <$> [1,2,3] <*> [10,100,1000]
+-- [10,100,1000,20,200,2000,30,300,3000]
+
+-- *Main Control.Applicative> concat [[1,2],[3,4]]
+-- [1,2,3,4]
+
+-- *Main Control.Applicative> [3,4,5] >>= \x -> [x, -x]
+-- [3,-3,4,-4,5,-5]
+
+-- *Main Control.Applicative> [] >>= \x -> ["bad","mad","rad"]
+-- []
+
+-- *Main Control.Applicative> [1,2,3] >>= \x -> []
+-- []
+
+
 -- to be continued
