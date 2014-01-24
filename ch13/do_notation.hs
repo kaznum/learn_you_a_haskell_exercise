e46a4edc7e4f4a24c0c1a9d173318ab644b3075c
Kazuya NUMATA <numata@gmail.com>
2014-01-24 12:27:12 +0900

    Implement ch13/do_notation examples (in progress)

--- ch13/do_notation.hs
+++ ch13/do_notation.hs
@@ -32,4 +32,55 @@ marySue = do
   x <- Just 9
   Just (x > 8)
 
+--- Pierre Returns
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) < 4 = Just (left + n, right)
+  | otherwise = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs (left - (right + n)) < 4 = Just (left, right + n)
+  | otherwise = Nothing
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
+
+
+routine :: Maybe Pole
+routine = do
+  start <- return (0, 0)
+  first <- landLeft 2 start
+  second <- landRight 2 first
+  landLeft 1 second
+
+-- *Main> routine
+-- Just (3,2)
+
+routine' :: Maybe Pole
+routine' =
+  case Just (0, 0) of
+    Nothing -> Nothing
+    Just start -> case landLeft 2 start of
+      Nothing -> Nothing
+      Just first -> case landRight 2 first of
+        Nothing -> Nothing
+        Just second -> landLeft 1 second
+
+-- *Main> routine'
+-- Just (3,2)
+
+routine'' :: Maybe Pole
+routine'' = do
+  start <- return (0, 0)
+  first <- landLeft 2 start
+  Nothing
+  second <- landRight 2 first
+  landLeft 1 second
+
+--- Pattern Matching and Failure
+
 -- to be continued
