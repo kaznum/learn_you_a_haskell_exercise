394626c7a607fdbc27212c38226f24b739254b1f
Kazuya Numata <numata@gmail.com>
2014-01-26 18:09:45 +0900

    Implement ch13/the_list_monad examples

--- ch13/the_list_monad.hs
+++ ch13/the_list_monad.hs
@@ -64,5 +64,42 @@ sevensOnly = do
 -- [7,17,27,37,47]
 
 --- A Knight's Quest
+type KnightPos = (Int, Int)
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c,r) = do
+  (c', r') <- [(c+2, r-1),(c+2, r+1),(c-2, r-1),(c-2, r+1)
+              ,(c+1, r-2),(c+1, r+2),(c-1,r-2),(c-1,r+2)]
+  guard (c' `elem` [1..8] && r' `elem` [1..8])
+  return (c',r')
 
--- to be continued
+moveKnight' :: KnightPos -> [KnightPos]
+moveKnight' (c, r) = filter onBoard
+            [(c+2, r-1),(c+2, r+1),(c-2, r-1),(c-2, r+1)
+            ,(c+1, r-2),(c+1, r+2),(c-1,r-2),(c-1,r+2)]
+            where onBoard (c, r) = c `elem` [1..8] && r `elem` [1..8]
+
+-- *Main Control.Monad> moveKnight (6,2)
+-- [(8,1),(8,3),(4,1),(4,3),(7,4),(5,4)]
+-- *Main Control.Monad> moveKnight (8,1)
+-- [(6,2),(7,3)]
+-- *Main Control.Monad> moveKnight' (6,2)
+-- [(8,1),(8,3),(4,1),(4,3),(7,4),(5,4)]
+-- *Main Control.Monad> moveKnight' (8,1)
+-- [(6,2),(7,3)]
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = do
+  first <- moveKnight start
+  second <- moveKnight first
+  moveKnight second
+
+in3' :: KnightPos -> [KnightPos]
+in3' start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
+
+-- *Main Control.Monad> (6, 2) `canReachIn3` (6,1)
+-- True
+-- *Main Control.Monad> (6, 2) `canReachIn3` (7, 3)
+-- False
