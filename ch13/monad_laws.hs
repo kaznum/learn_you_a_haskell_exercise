73c69b2c6b1250da6b6a7b724569a1f7ff827e29
Kazuya Numata <numata@gmail.com>
2014-01-27 00:07:24 +0900

    Implement ch13/monad_laws examples (in progress)

--- ch13/monad_laws.hs
+++ ch13/monad_laws.hs
@@ -1,1 +1,16 @@
+--- Left Identity
+
+-- *Main Control.Monad> return 3 >>= (\x -> Just (x+100000))
+-- Just 100003
+-- *Main Control.Monad> (\x -> Just (x+100000)) 3
+-- Just 100003
+
+-- *Main Control.Monad> return "WoM" >>= (\x -> [x,x,x])
+-- ["WoM","WoM","WoM"]
+-- *Main Control.Monad> (\x -> [x,x,x]) "WoW"
+-- ["WoW","WoW","WoW"]
+
+--- Right Identity
+
+
 -- to be continued
