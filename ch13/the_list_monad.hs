ad9e950eff52d58c22a02bd96e2604fe440dfd7f
Kazuya Numata <numata@gmail.com>
2014-01-26 12:44:42 +0900

    Implement ch13/the_list_monad examples (in progress)

--- ch13/the_list_monad.hs
+++ ch13/the_list_monad.hs
@@ -1,4 +1,6 @@
--- *Main> import Control.Applicative
+import Control.Applicative
+import Control.Monad
+
 -- *Main Control.Applicative> (*) <$> [1,2,3] <*> [10,100,1000]
 -- [10,100,1000,20,200,2000,30,300,3000]
 
@@ -31,5 +33,36 @@ listOfTuples = do
 -- [(1,'a'),(1,'b'),(2,'a'),(2,'b')]
 
 --- MonadPlus and the guard Function
+-- *Main> [x | x <- [1..50], '7' `elem` show x]
+-- [7,17,27,37,47]
+
+-- *Main Control.Monad> guard (5 > 2) :: Maybe ()
+-- Just ()
+-- *Main Control.Monad> guard (1 > 2) :: Maybe ()
+-- Nothing
+-- *Main Control.Monad> guard (5 > 2) :: [()]
+-- [()]
+-- *Main Control.Monad> guard (1 > 2) :: [()]
+-- []
+-- *Main Control.Monad> [1..50] >>= (\x -> guard ('7' `elem` show x) >> return x)
+-- [7,17,27,37,47]
+
+-- *Main Control.Monad> guard (5 > 2) >> return "cool" :: [String]
+-- ["cool"]
+-- *Main Control.Monad> guard (1 > 2) >> return "cool" :: [String]
+-- []
+
+sevensOnly :: [Int]
+sevensOnly = do
+  x <- [1..50]
+  guard ('7' `elem` show x)
+  return x
+
+-- *Main Control.Monad> sevensOnly
+-- [7,17,27,37,47]
+-- *Main Control.Monad> [x|x<- [1..50], '7' `elem` show x]
+-- [7,17,27,37,47]
+
+--- A Knight's Quest
 
 -- to be continued
