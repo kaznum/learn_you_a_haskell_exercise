af836e7c62bffee0c4d2d082d33bcc7a96867dd7
Kazuya NUMATA <numata@gmail.com>
2014-01-27 12:52:16 +0900

    Implement ch13/monad_laws examples

--- ch13/monad_laws.hs
+++ ch13/monad_laws.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 --- Left Identity
 
 -- *Main Control.Monad> return 3 >>= (\x -> Just (x+100000))
@@ -22,5 +24,38 @@
 
 --- Associativity
 
+-- From the last section
+type Birds = Int
+type Pole = (Birds, Birds)
 
--- to be continued
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+  | abs ((left + n) - right) < 4 = Just (left + n, right)
+  | otherwise = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+  | abs (left - (right + n)) < 4 = Just (left, right + n)
+  | otherwise = Nothing
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
+
+-- End of the codes from the last section
+
+-- *Main> return (0, 0) >>= landRight 2 >>= landLeft 2 >>= landRight 2
+-- Just (2,4)
+-- *Main> ((return (0, 0) >>= landRight 2) >>= landLeft 2) >>= landRight 2
+-- Just (2,4)
+
+-- *Main> return (0, 0) >>= (\x -> landRight 2 x >>= (\y -> landLeft 2 y >>= (\z -> landRight 2 z)))
+-- Just (2,4)
+
+-- *Main> :t (<=<)
+-- (<=<) :: Monad m => (b -> m c) -> (a -> m b) -> a -> m c
+-- *Main> let f x = [x, -x]
+-- *Main> let g x = [x*3, x*2]
+-- *Main> let h = f <=< g
+-- *Main> h 3
+-- [9,-9,6,-6]
+
