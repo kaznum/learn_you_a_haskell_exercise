733933a5c8647985aff5ada09dbad3070b7de278
Kazuya NUMATA <numata@gmail.com>
2014-01-28 10:47:42 +0900

    Implement ch14/writer_i_hardly_knew_her examples (in progress)

--- ch14/writer_i_hardly_knew_her.hs
+++ ch14/writer_i_hardly_knew_her.hs
@@ -1,1 +1,32 @@
+isBigGang :: Int -> Bool
+isBigGang x = x > 9
+
+-- *Main> isBigGang 3
+-- False
+-- *Main> isBigGang 10
+-- True
+
+isBigGang' :: Int -> (Bool, String)
+isBigGang' x = (x > 9, "Compared gang size to 9.")
+
+-- *Main> isBigGang' 3
+-- (False,"Compared gang size to 9.")
+-- *Main> isBigGang' 30
+-- (True,"Compared gang size to 9.")
+
+applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog (x, log) f = let (y, newlog) = f x in (y, log ++ newlog)
+
+-- *Main> (3, "Smallish gang.") `applyLog` isBigGang'
+-- (False,"Smallish gang.Compared gang size to 9.")
+-- *Main> (30, "A freaking platoon.") `applyLog` isBigGang'
+-- (True,"A freaking platoon.Compared gang size to 9.")
+
+-- *Main> ("Tobin", "Got outlaw name.") `applyLog` (\x -> (length x, "Apply length."))
+-- (5,"Got outlaw name.Apply length.")
+-- *Main> ("Bathcat", "Got outlaw name.") `applyLog` (\x -> (length x, "Apply length."))
+-- (7,"Got outlaw name.Apply length.")
+
+--- Monoids to the Rescue
+
 -- to be continued
