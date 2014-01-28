10665a14b734f394720f6a3a76de1905cf202093
Kazuya NUMATA <numata@gmail.com>
2014-01-28 15:31:43 +0900

    Implement ch14/reader_ugh_not_this_joke_again examples (in progress)

--- ch14/reader_ugh_not_this_joke_again.hs
+++ ch14/reader_ugh_not_this_joke_again.hs
@@ -1,1 +1,19 @@
+import Control.Applicative
+
+-- *Main> let f = (*5)
+-- *Main> let g = (+3)
+-- *Main> (fmap f g) 8
+-- 55
+-- *Main Control.Applicative> let f = (+) <$> (*2) <*> (+10)
+-- *Main Control.Applicative> f 3
+-- 19
+
+---- (+) <$> (*2) <*> (+10)
+---- (fmap (+) (*2)) <*> (+10)
+---- (\x -> (+) (x*2)) <*> (\x -> (x+10))
+---- (\x -> (+) (x*2) (x+10))
+
+
+--- Functions As Monads
+
 -- to be continued
