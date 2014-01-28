6378be4964e530a8595cb96d5b3967b02d30c52c
Kazuya NUMATA <numata@gmail.com>
2014-01-28 12:57:43 +0900

    Implement ch14/writer_i_hardly_knew_her examples (in progress)

--- ch14/writer_i_hardly_knew_her.hs
+++ ch14/writer_i_hardly_knew_her.hs
@@ -107,5 +107,34 @@ multWithLog' = do
 
 
 --- Adding Logging to Programs
+gcd' :: Int -> Int -> Int
+gcd' a b
+  | b == 0 = a
+  | otherwise = gcd' b (a `mod` b)
+
+-- *Main> gcd' 8 3
+-- 1
+
+gcd'' :: Int -> Int -> Writer [String] Int
+gcd'' a b
+  | b == 0 = do
+    tell ["Finished with " ++ show a]
+    return a
+  | otherwise = do
+    tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    gcd'' b (a `mod` b)
+
+-- *Main> runWriter (gcd'' 8 3)
+-- (1,["8 mod 3 = 2","3 mod 2 = 1","2 mod 1 = 0","Finished with 1"])
+
+-- *Main> fst $ runWriter (gcd'' 8 3)
+-- 1
+-- *Main> mapM_ putStrLn $ snd $ runWriter (gcd'' 8 3)
+-- 8 mod 3 = 2
+-- 3 mod 2 = 1
+-- 2 mod 1 = 0
+-- Finished with 1
+
+--- Inefficient List Construction
 
 -- to be continued
