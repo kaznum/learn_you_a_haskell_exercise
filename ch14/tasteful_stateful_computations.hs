a741aadba7aa147631753b345fd5e4b632f3743e
Kazuya Numata <numata@gmail.com>
2014-01-29 00:14:34 +0900

    Implement ch14/tasteful_stateful_computations examples (in progress)

--- ch14/tasteful_stateful_computations.hs
+++ ch14/tasteful_stateful_computations.hs
@@ -13,4 +13,24 @@ threeCoins gen =
 
 --- Stateful Computations
 
+--- Stacks and Stones
+type Stack = [Int]
+
+pop :: Stack -> (Int, Stack)
+pop (x:xs) = (x, xs)
+
+push :: Int -> Stack -> ((), Stack)
+push a xs = ((), a:xs)
+
+stackManip :: Stack -> (Int, Stack)
+stackManip stack = let
+           ((), newStack1) = push 3 stack
+           (a, newStack2) = pop newStack1
+           in pop newStack2
+
+-- *Main> stackManip [5,8,2,1]
+-- (5,[8,2,1])
+
+--- The State Monad
+
 -- to be continued
