370a3c606d1777482d4ffc50c7ed77f420d09ae9
Kazuya Numata <numata@gmail.com>
2014-01-28 23:56:12 +0900

    Implement ch14/tasteful_stateful_computations examples (in progress)

--- ch14/tasteful_stateful_computations.hs
+++ ch14/tasteful_stateful_computations.hs
@@ -1,1 +1,16 @@
+import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+           let (firstCoin, newGen) = random gen
+               (secondCoin, newGen') = random newGen
+               (thirdCoin, newGen'') = random newGen'
+           in (firstCoin, secondCoin, thirdCoin)
+
+
+-- *Main> threeCoins (mkStdGen 152)
+-- (True,True,False)
+
+--- Stateful Computations
+
 -- to be continued
