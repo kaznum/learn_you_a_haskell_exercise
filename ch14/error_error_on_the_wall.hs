dc263b80e37c90b0a74ad9453e4f56a4e1a1c3b8
Kazuya NUMATA <numata@gmail.com>
2014-01-29 12:35:37 +0900

    Implement ch14/error_error_on_the_wall examples

--- ch14/error_error_on_the_wall.hs
+++ ch14/error_error_on_the_wall.hs
@@ -1,1 +1,22 @@
--- to be continued
+import Control.Monad.Error
+
+-- *Main> :t Right 4
+-- Right 4 :: Num b => Either a b
+-- *Main> :t Left "out of cheese error"
+-- Left "out of cheese error" :: Either [Char] b
+
+-- *Main> strMsg "boom!" :: String
+-- "boom!"
+
+-- *Main> Left "boom" >>= \x -> return (x+1)
+-- Left "boom"
+-- *Main> Left "boom" >>= \x -> Left "no way!"
+-- Left "boom"
+-- *Main> Right 100 >>= \x -> Left "no way!"
+-- Left "no way!"
+
+-- *Main> Right 3 >>= \x -> return (x + 100)
+-- Right 103
+
+-- *Main> Right 3 >>= \x -> return (x + 100) :: Either String Int
+-- Right 103
