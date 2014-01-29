0e2a0bbb1a7e97549a178b9a86df673876a73ba4
Kazuya NUMATA <numata@gmail.com>
2014-01-29 13:09:22 +0900

    Implement ch14/some_useful_monadic_functions examples (in progress)

--- ch14/some_useful_monadic_functions.hs
+++ ch14/some_useful_monadic_functions.hs
@@ -1,1 +1,30 @@
+import Control.Monad
+import Control.Monad.Writer
+import Control.Monad.State
+
+-- liftM and Friends
+-- *Main> liftM (*3) (Just 8)
+-- Just 24
+-- *Main> fmap (*3) (Just 8)
+-- Just 24
+-- *Main> runWriter $ liftM not $ writer (True, "chickpeas")
+-- (False,"chickpeas")
+-- *Main> runWriter $ fmap not $ writer (True, "chickpeas")
+-- (False,"chickpeas")
+
+type Stack = [Int]
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x, xs)
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
+
+-- *Main> runState (liftM (+100) pop) [1,2,3,4]
+-- (101,[2,3,4])
+-- *Main> runState (fmap (+100) pop) [1,2,3,4]
+-- (101,[2,3,4])
+-- *Main> :t liftM
+-- liftM :: Monad m => (a1 -> r) -> m a1 -> m r
+-- *Main> :t fmap
+-- fmap :: Functor f => (a -> b) -> f a -> f b
+
 -- to be continued
