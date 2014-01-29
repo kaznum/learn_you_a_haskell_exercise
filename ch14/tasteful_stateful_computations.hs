399a0b768a25701b808cfbeb13ca137706bc0efa
Kazuya NUMATA <numata@gmail.com>
2014-01-29 10:32:37 +0900

    Implement ch14/tasteful_stateful_computations examples (in progress)

--- ch14/tasteful_stateful_computations.hs
+++ ch14/tasteful_stateful_computations.hs
@@ -1,4 +1,5 @@
 import System.Random
+import Control.Monad.State
 
 threeCoins :: StdGen -> (Bool, Bool, Bool)
 threeCoins gen =
@@ -16,21 +17,69 @@ threeCoins gen =
 --- Stacks and Stones
 type Stack = [Int]
 
-pop :: Stack -> (Int, Stack)
-pop (x:xs) = (x, xs)
+pop' :: Stack -> (Int, Stack)
+pop' (x:xs) = (x, xs)
 
-push :: Int -> Stack -> ((), Stack)
-push a xs = ((), a:xs)
+push' :: Int -> Stack -> ((), Stack)
+push' a xs = ((), a:xs)
 
 stackManip :: Stack -> (Int, Stack)
 stackManip stack = let
-           ((), newStack1) = push 3 stack
-           (a, newStack2) = pop newStack1
-           in pop newStack2
+           ((), newStack1) = push' 3 stack
+           (a, newStack2) = pop' newStack1
+           in pop' newStack2
 
 -- *Main> stackManip [5,8,2,1]
 -- (5,[8,2,1])
 
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x, xs)
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
+
 --- The State Monad
+stackManip' :: State Stack Int
+stackManip' = do
+  push 3
+  a <- pop
+  pop
+
+-- *Main> runState stackManip' [5,8,2,1]
+-- (5,[8,2,1])
+
+stackManip'' :: State Stack Int
+stackManip'' = do
+  push 3
+  pop
+  pop
+
+-- *Main> runState stackManip'' [5,8,2,1]
+-- (5,[8,2,1])
+
+stackStuff :: State Stack ()
+stackStuff = do
+  a <- pop
+  if a == 5
+    then push 5
+    else do
+      push 3
+      push 8
+
+-- *Main> runState stackStuff [9,0,2,1,0]
+-- ((),[8,3,0,2,1,0])
+
+moreStack :: State Stack ()
+moreStack = do
+  a <- stackManip''
+  if a == 100
+    then stackStuff
+    else return ()
+
+-- *Main> runState moreStack [9,0,2,1,0]
+-- ((),[0,2,1,0])
+-- *Main> runState moreStack [100,0,2,1,0]
+-- ((),[8,3,2,1,0])
+
+--- Getting and Setting State
 
 -- to be continued
