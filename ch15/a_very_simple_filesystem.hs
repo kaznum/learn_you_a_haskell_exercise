62f3d7954e64255bef34ee36331c67e54f8cf6dd
Kazuya NUMATA <numata@gmail.com>
2014-01-31 16:26:30 +0900

    Implement ch15/a_very_simple_filesystem examples (in progress)

--- ch15/a_very_simple_filesystem.hs
+++ ch15/a_very_simple_filesystem.hs
@@ -1,3 +1,5 @@
+import Data.List (break)
+
 type Name = String
 type Data = String
 data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
@@ -23,5 +25,34 @@ myDisk =
                 ]
             ]
         ]
+
+--- Making a Zipper for Our Filesystem
+
+data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
+
+type FSZipper = (FSItem, [FSCrumb])
+
+fsUp :: FSZipper -> FSZipper
+fsUp (item, FSCrumb name ls rs:bs) = (Folder name (ls ++ [item] ++ rs), bs)
+
+fsTo :: Name -> FSZipper -> FSZipper
+fsTo name (Folder folderName items, bs) =
+  let (ls, item:rs) = break (nameIs name) items
+  in (item, FSCrumb folderName ls rs:bs)
+
+nameIs :: Name -> FSItem -> Bool
+nameIs name (Folder folderName _) = name == folderName
+nameIs name (File fileName _) = name == fileName
+
+x -: f = f x
+
+-- *Main> let newFocus = (myDisk, []) -: fsTo "pics" -: fsTo "skull_man(scary).bmp"
+-- *Main> fst newFocus
+-- File "skull_man(scary).bmp" "Yikes!"
+-- *Main> let newFocus2 = newFocus -: fsUp -: fsTo "watermelon_smash.gif"
+-- *Main> fst newFocus2
+-- File "watermelon_smash.gif" "smash!!"
+
+--- Manipulating a Filesystem
 
 -- to be continued
