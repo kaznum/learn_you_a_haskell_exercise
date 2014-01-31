b22a3878eebbfd0311477f42485034048718cbf7
Kazuya NUMATA <numata@gmail.com>
2014-01-31 15:50:33 +0900

    Implement ch15/focusing_on_lists examples

--- ch15/focusing_on_lists.hs
+++ ch15/focusing_on_lists.hs
@@ -1,1 +1,19 @@
--- to be continued
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+type ListZipper a = ([a], [a])
+
+goForward :: ListZipper a -> ListZipper a
+goForward (x:xs, bs) = (xs, x:bs)
+
+goBack :: ListZipper a -> ListZipper a
+goBack (xs, b:bs) = (b:xs, bs)
+
+-- *Main> let xs = [1,2,3,4]
+-- *Main> goForward (xs, [])
+-- ([2,3,4],[1])
+-- *Main> goForward ([2,3,4],[1])
+-- ([3,4],[2,1])
+-- *Main> goForward ([3,4],[2,1])
+-- ([4],[3,2,1])
+-- *Main> goBack ([4],[3,2,1])
+-- ([3,4],[2,1])
