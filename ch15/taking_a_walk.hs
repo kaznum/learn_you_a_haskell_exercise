eebd577493bdf63c8e4036ea31e61e4f7f10a3c0
Kazuya NUMATA <numata@gmail.com>
2014-01-31 13:32:20 +0900

    Implement ch15/taking_a_walk examples (in progress)

--- ch15/taking_a_walk.hs
+++ ch15/taking_a_walk.hs
@@ -24,4 +24,34 @@ freeTree =
             )
         )
 
+changeToP :: Tree Char -> Tree Char
+changeToP (Node x l (Node y (Node _ m n) r)) = Node x l (Node y (Node 'P' m n) r)
+
+-- *Main> changeToP freeTree
+-- Node 'P' (Node 'O' (Node 'L' (Node 'N' Empty Empty) (Node 'T' Empty Empty)) (Node 'Y' (Node 'S' Empty Empty) (Node 'A' Empty Empty))) (Node 'L' (Node 'P' (Node 'C' Empty Empty) (Node 'R' Empty Empty)) (Node 'A' (Node 'A' Empty Empty) (Node 'C' Empty Empty)))
+
+data Direction = L | R deriving (Show)
+type Directions = [Direction]
+
+changeToP' :: Directions -> Tree Char -> Tree Char
+changeToP' (L:ds) (Node x l r) = Node x (changeToP' ds l) r
+changeToP' (R:ds) (Node x l r) = Node x l (changeToP' ds r)
+changeToP' [] (Node _ l r) = Node 'P' l r
+
+-- *Main> changeToP' [R,L] freeTree
+-- Node 'P' (Node 'O' (Node 'L' (Node 'N' Empty Empty) (Node 'T' Empty Empty)) (Node 'Y' (Node 'S' Empty Empty) (Node 'A' Empty Empty))) (Node 'L' (Node 'P' (Node 'C' Empty Empty) (Node 'R' Empty Empty)) (Node 'A' (Node 'A' Empty Empty) (Node 'C' Empty Empty)))
+
+elemAt :: Directions -> Tree a -> a
+elemAt (L:ds) (Node _ l _) = elemAt ds l
+elemAt (R:ds) (Node _ _ r) = elemAt ds r
+elemAt [] (Node x _ _) = x
+
+-- *Main> let newTree = changeToP' [R,L] freeTree
+-- *Main> elemAt [R,L] newTree
+-- 'P'
+
+--- A Trail of Breadcrumbs
+
+
+
 -- to be continued
